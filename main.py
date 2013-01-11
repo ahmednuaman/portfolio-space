497b38792d56d047c5811120ded8f8f40a2d7c3d
Ahmed Nuaman <ahmed@ahmednuaman.com>
2013-01-11 15:23:41 +0000

    set up routes

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import webapp2
 import webapp2_config
 
-from app.controller import admin_controller, page_controller
+from app.controller import admin_controller, manage_controller, page_controller, signup_controller
 
 config = webapp2_config
 
 app = webapp2.WSGIApplication([
-  (admin_controller.decorator.callback_path, admin_controller.decorator.callback_handler()),
+  (signup_controller.decorator.callback_path, signup_controller.decorator.callback_handler()),
   ('/admin/', admin_controller.AdminController),
+  ('/signup/', signup_controller.SignupController),
+  ('/manage/', manage_controller.ManageController),
   (r'/([^\/]+)?', page_controller.PageController)
 ], config=config)
