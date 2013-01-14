7a160a950be1a79990b732e3c7014e2f6a3c839e
Ahmed Nuaman <ahmed@ahmednuaman.com>
2013-01-14 13:00:29 +0000

    added oauth decorator for google oauth

--- app/controller/signup_controller.py
+++ app/controller/signup_controller.py
@@ -1,4 +1,14 @@
+import app_config
+
 from app.controller.base_controller import BaseController
+from oauth2client.appengine import OAuth2Decorator
+
+# set up decorator
+decorator = OAuth2Decorator(
+  client_id=app_config.client_id,
+  client_secret=app_config.client_secret,
+  scope='https://www.googleapis.com/auth/userinfo'
+)
 
 class SignupController(BaseController):
   def get(self):
